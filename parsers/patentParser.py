3998fe7e6526650f43f8d8d0973b4acdeb3eaaa3
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-03-26 21:30:44 +0300

    Доработка парсера

--- parsers/patentParser.py
+++ parsers/patentParser.py
@@ -1,5 +1,32 @@
 from bs4 import BeautifulSoup as BS
+import os
+from models.parsedPatent import ParsedPatent
 from models.patent import Patent
+
+
+def save_to_file(path: str, patent_title: str, ext: str, data: str):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    filePath = path + "/" + patent_title + ext
+    try:
+        with open(filePath, 'w', encoding="utf-8") as file:
+            file.write(data.strip())
+    except EnvironmentError:
+        print("Ошибка при записи в файл:" + filePath)
+    return filePath
+
+
+def get_text_field(parent_name, field_name: str):
+    field = parent_name.find(field_name)
+    try:
+        if field:
+            text_field = field.text.strip()
+            return text_field
+        else:
+            return None
+    except AttributeError:
+        print("Ошибка при обращении к полю" + field_name)
+        return None
 
 
 def splitter(text: str):
@@ -9,33 +36,55 @@ def splitter(text: str):
     return splitted
 
 
-def parse_XML(splitted_xml):
-    patents = []
-    for number, text in enumerate(splitted_xml):
+def parse_XML(text):
+    patent_reference = ''
+    try:
         soup = BS(text, 'lxml')
         app_ref_tmp = soup.find("application-reference")
         if app_ref_tmp:
-            application_reference = {'doc_number': app_ref_tmp.find("doc-number").text,
-                                     'country': app_ref_tmp.find("country").text,
-                                     'date': app_ref_tmp.find("date").text}
+            application_reference = {'doc_number': get_text_field(app_ref_tmp, "doc-number"),
+                                     'country': get_text_field(app_ref_tmp, "country"),
+                                     'date': get_text_field(app_ref_tmp, "date")}
         else:
             application_reference = None
 
         pub_ref_tmp = soup.find("publication-reference")
         if pub_ref_tmp:
-            publication_reference = {'doc-number': pub_ref_tmp.find("doc-number").text,
-                                     'country': pub_ref_tmp.find("country").text,
-                                     'date': pub_ref_tmp.find("date").text}
+            publication_reference = {'doc-number': get_text_field(pub_ref_tmp, "doc-number"),
+                                     'country': get_text_field(pub_ref_tmp, "country"),
+                                     'date': get_text_field(pub_ref_tmp, "date"),
+                                     'kind': get_text_field(pub_ref_tmp, "kind")}
+            patent_reference = publication_reference["country"] + publication_reference["doc-number"] \
+                               + publication_reference["kind"]
         else:
             publication_reference = None
+
+        invention_title = soup.find("invention-title")
+        if invention_title:
+            title = invention_title.text.strip()
+        else:
+            title = None
+
+        inventors = []
+        applicants = soup.find("applicants")
+        if applicants:
+            for content in applicants.contents:
+                if content != '\n':
+                    inventor = {'first-name': get_text_field(content, "first-name"),
+                                'last-name': get_text_field(content, "last-name")}
+                    inventors.append(inventor)
 
         abstract_tmp = soup.find("abstract")
         if abstract_tmp:
             abstract = abstract_tmp.text
+        else:
+            abstract = None
 
         description_tmp = soup.find("description")
         if description_tmp:
             description = description_tmp.text
+        else:
+            description = None
 
         claims = [x.text for x in soup.findAll("claim-text")]
 
@@ -68,20 +117,63 @@ def parse_XML(splitted_xml):
                     else:
                         continue
 
-        patents.append(Patent(application_reference, publication_reference, str(classification_type),
-                              str(main_classification), abstract, description, claims))
+        return ParsedPatent(application_reference, publication_reference, str(classification_type),
+                           str(main_classification), title, inventors, abstract, description, claims)
 
-    return patents
+    except Exception:
+        print("Ошибка при парсинге патента " + patent_reference)
+        return None
+
+
+def save_patent(patent: ParsedPatent, path: str):
+    result = {}
+    filename = patent.publication_reference["country"] + patent.publication_reference["doc-number"] \
+               + patent.publication_reference["kind"]
+
+    result['abstractPath'] = None
+    result['descriptionPath'] = None
+    result['claimsPath'] = None
+    if patent.abstract:
+        abstract_path = save_to_file(path + '/abstract', filename,
+                                     '.txt', patent.abstract)
+        result['abstractPath'] = abstract_path
+    if patent.description:
+        description_path = save_to_file(path + '/description', filename,
+                                        '.txt', patent.description)
+        result['descriptionPath'] = description_path
+    if patent.claims:
+        claims_path = save_to_file(path + '/claims', filename,
+                                   '.txt', '\n'.join(patent.claims))
+        result['claimsPath'] = claims_path
+    return result
+
+
+def map_patent(parsed_patent: ParsedPatent, paths):
+    patent_reference = parsed_patent.publication_reference["country"] + parsed_patent.publication_reference["doc-number"] \
+                       + parsed_patent.publication_reference["kind"]
+    if parsed_patent.inventors and len(parsed_patent.inventors) > 0:
+        inventors = ','.join(str(e['first-name']) + " " + str(e['last-name']) for e in parsed_patent.inventors)
+    else:
+        inventors = ""
+    return Patent(patent_reference, parsed_patent.main_classification_type,
+                        parsed_patent.main_classification, parsed_patent.title, inventors, paths['abstractPath'],
+                        paths['descriptionPath'], paths['claimsPath'])
 
 
 def main():
     file = 'C:/Users/mrkol/Documents/patents/unpack/ipg100105.xml'
+    path = 'C:/Users/mrkol/Documents'
     with open(file) as f:
         xml = f.read()
     splitted_xml = splitter(xml)
-    patents = parse_XML(splitted_xml)
-    for p in patents:
-        print(p.description)
+    print(len(splitted_xml))
+    iter = 0
+    while iter < 10:
+        parsed_patent = parse_XML(splitted_xml[iter])
+        save_files = save_patent(parsed_patent, path)
+        patent = map_patent(parsed_patent, save_files)
+        iter += 1
+    print(patent.title)
 
 
 if __name__ == '__main__':
