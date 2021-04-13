857d1d217999e9959eea338f5f6ccfd23cf3e38b
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-04-13 09:06:07 +0300

    добавление класса sao

---
+++ models/sao.py
@@ -0,0 +1,9 @@
+class Sao:
+    """Класс sao (Subject-Action-Object)"""
+
+    def __init__(self, id, subject, action, obj, patent_id):
+        self.id = id
+        self.subject = subject
+        self.action = action
+        self.object = obj
+        self.patent_id = patent_id
