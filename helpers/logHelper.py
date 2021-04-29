29212bf6ffee23c91c32158b53afa4aafa8809f6
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-04-29 16:57:56 +0300

    создал функцию для записи sao в файл

---
+++ helpers/logHelper.py
@@ -0,0 +1,20 @@
+from typing import List
+from models.sao import Sao
+
+
+def record_sao_log(patent_number: str, description_text: str, sentences: List[str], saos: List[Sao], filePath: str):
+    try:
+        with open(filePath, 'a', encoding="utf-8") as file:
+            file.write(patent_number + '\n')
+            file.write(description_text + '\n\n')
+            file.write('Отобранные для извлечения SAO предложения:\n')
+            for sentence in sentences:
+                file.write(sentence + '\n')
+            file.write('\nSAO:\n')
+            for sao in saos:
+                file.write('Subject: ' + sao.subject + '\n')
+                file.write('Action: ' + sao.action + '\n')
+                file.write('Object: ' + sao.object + '\n\n')
+            file.write('\n')
+    except EnvironmentError:
+        print("Ошибка при записи в файл:" + filePath)
