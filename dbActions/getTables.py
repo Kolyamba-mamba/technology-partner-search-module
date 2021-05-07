cfbba062da5eb7b7934a018963178af791ac7c9b
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-05-07 15:58:59 +0300

    создал sql скрипты

--- dbActions/getTables.py
+++ dbActions/getTables.py
@@ -17,3 +17,14 @@ def get_cursor(connection, table_name):
     query = f"SELECT * FROM {table_name}"
     cursor = get_cursor_query(connection, query)
     return cursor
+
+
+def get_entities_by_condition(connection, table_name, condition):
+    query = f"SELECT * FROM {table_name} WHERE {condition} ORDER BY id"
+    elements = execute_select_all_query(connection, query)
+    return elements
+
+def get_abstract_and_descr_path(connection):
+    query = f"SELECT abstract_path,description_path FROM patents"
+    elements = execute_select_all_query(connection, query)
+    return elements
